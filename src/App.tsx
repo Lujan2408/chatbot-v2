913463d158ba2d97a6e780e1bc76bdf0a474425f
Jose David <lujan9495@gmail.com>
2025-05-31 20:19:29 -0500

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer"
 import GenerateAI from "./components/GenerateAI"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <GenerateAI />
+      <Footer />       
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 py-6">
+      <div className="w-full max-w-7xl mx-auto px-4">
+        <div className="flex flex-col items-center justify-center text-center">
+          <p className="text-sm sm:text-base text-slate-300">
+            Developed by{" "}
+            <span className="font-bold">Jose David Cardona Luján</span>
+            <br className="hidden sm:block" />
+            <span className="block sm:inline">&nbsp;&copy; 2025.</span>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
